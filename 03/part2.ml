d0921ffa53bfc3b52f3262fcdfd2c952843afaa5
Piotr Łukomiak <piotr.lukomiak@gmail.com>
2023-12-03 21:09:56 +0100

    Day 3

---
+++ 03/part2.ml
@@ -0,0 +1,65 @@
+open Base
+open Stdio
+
+module Point = struct
+  module T = struct
+    type t = int * int [@@deriving compare, sexp_of]
+  end
+
+  include T
+  include Comparable.Make (T)
+end
+
+let moves =
+  [(-1, 1); (0, 1); (1, 1); (-1, 0); (1, 0); (-1, -1); (0, -1); (1, -1)]
+
+let is_symbol c = Char.(c = '*')
+
+let () =
+  let lines = In_channel.read_lines "input.txt" in
+  let symbols =
+    List.foldi
+      ~f:(fun j acc line ->
+        List.foldi
+          ~f:(fun i acc c -> if is_symbol c then (i, j) :: acc else acc)
+          ~init:acc (String.to_list line))
+      ~init:[] lines
+  in
+  let _, numbers =
+    List.foldi
+      ~f:(fun j acc line ->
+        List.foldi
+          ~f:(fun i ((current_number, positions), acc) c ->
+            if Char.is_digit c then
+              ((current_number ^ String.of_char c, (i, j) :: positions), acc)
+            else
+              ( ("", []),
+                if String.is_empty current_number then acc
+                else (Int.of_string current_number, positions) :: acc ))
+          ~init:acc (String.to_list line))
+      ~init:(("", []), [])
+      lines
+  in
+  let gears =
+    List.filter_map
+      ~f:(fun symbol ->
+        let ratios =
+          List.filter_map
+            ~f:(fun (value, positions) ->
+              let neighbors =
+                Set.of_list (module Point)
+                @@ List.concat_map
+                     ~f:(fun (x, y) ->
+                       List.map ~f:(fun (dx, dy) -> (x + dx, y + dy)) moves)
+                     positions
+              in
+              if Set.mem neighbors symbol then Some value else None)
+            numbers
+        in
+        match ratios with
+        | [x; y] -> Some (x * y)
+        | _ -> None)
+      symbols
+  in
+  let sum = List.sum ~f:(fun x -> x) (module Int) gears in
+  printf "%d\n" sum
